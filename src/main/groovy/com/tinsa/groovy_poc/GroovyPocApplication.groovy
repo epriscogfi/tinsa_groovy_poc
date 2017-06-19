77462aafb5077a5f69a81b0b9bf3bc3a294c9972
eprisco <epriscom@gmail.com>
2017-06-19 10:38:19 +0200

    Initial commit

---
+++ src/main/groovy/com/tinsa/groovy_poc/GroovyPocApplication.groovy
@@ -0,0 +1,12 @@
+package com.tinsa.groovy_poc
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GroovyPocApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run GroovyPocApplication, args
+	}
+}
---
+++ src/main/groovy/com/tinsa/groovy_poc/utils/Constantes.groovy
@@ -0,0 +1,60 @@
+package com.tinsa.groovy_poc.utils
+
+/**
+ * Created by usuario on 19/06/2017.
+ */
+class Constantes {
+/** Constante MAIL. */
+    static final String MAIL = "MAIL"
+
+    /** Constante SMS. */
+    static final String SMS = "SMS"
+
+    /** Constante FAX. */
+    static final String FAX = "FAX"
+
+    /** Constante WHATCHA. */
+    static final String WHATCHA = "WHATCHA"
+
+    /** Constante SERVICE_SMS_URL. */
+    static final String SERVICE_SMS_URL = "http://localhost:9100/tinsa/sms"
+
+    /** Constante PARAM_PHONE. */
+    static final String PARAM_PHONE = "phone"
+
+    /** Constante PARAM_MESSAGE. */
+    static final String PARAM_MESSAGE = "message"
+
+    /** Constante RESULT_OK. */
+    static final int RESULT_OK = 200
+
+    /** Constante ESTADO_ENVIADO. */
+    static final int ESTADO_ENVIADO = 1
+
+    /** Constante ESTADO_NO_ENVIADO. */
+    static final int ESTADO_NO_ENVIADO = 0
+
+    /** Constante DESTINO. */
+    static final String DESTINO = "666251478"
+
+    /** Constante TIPO_ENVIO. */
+    static final String TIPO_ENVIO = "SMS"
+
+    /** Constante MENSAJE. */
+    static final String MENSAJE = "Mensaje"
+
+    /** Constante ESTADO. */
+    static final Integer ESTADO = 1
+
+    /** Constante OTRO_DESTINO. */
+    static final String OTRO_DESTINO = "677258547"
+
+    /** Constante OTRO_TIPO_ENVIO. */
+    static final String OTRO_TIPO_ENVIO = "MAIL"
+
+    /** Constante OTRO_MENSAJE. */
+    static final String OTRO_MENSAJE = "Otro mensaje"
+
+    /** Constante OTRO_ESTADO. */
+    static final Integer OTRO_ESTADO = 0
+}
